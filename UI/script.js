49e2b8888f2264ce17f8e9faed829ebf98196f09
Александр А. Покидов <alexander.pokidov@pk.razional.ru>
2023-03-24 16:55:52 +0300

    update

--- UI/script.js
+++ UI/script.js
@@ -1,43 +1,41 @@
 function listenerVisual() {
-  const audio = document.querySelector('.audio');
-  const canvas = document.querySelector('.visual').getContext("2d");
-  const WIDTH = 500;
-  const HEIGHT = 400;
+  const audio = document.querySelector(".audio");
+  const canvas = document.querySelector(".visual").getContext("2d");
+  let WIDTH = canvas.canvas.clientWidth;
+  let HEIGHT = canvas.canvas.clientHeight;
   let bufferLength = null;
   let drawVisual = null;
   let dataArray = null;
   let analyser = null;
-  console.log('audio', audio)
+  console.log("audio", canvas);
 
   let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  
 
-  audio.addEventListener('play', (e) => {
-    console.log('event', e)
-    
+  audio.addEventListener("play", (e) => {
+    console.log("event", e);
+
     let stream = audio.captureStream();
-    console.log('stream', stream)
+    console.log("stream", stream);
     analyser = audioCtx.createAnalyser();
     source = audioCtx.createMediaStreamSource(stream);
-    console.log('audio', source)
+    console.log("audio", source);
     source.connect(analyser);
     analyser.fftSize = 2048;
     bufferLength = analyser.frequencyBinCount;
     dataArray = new Uint8Array(bufferLength);
-    console.log('dataArray', dataArray);
+    console.log("dataArray", dataArray);
     analyser.getByteFrequencyData(dataArray);
     // analyser.connect(distortion);
-    // distortion.connect(audioCtx.destination)
+    /// distortion.connect(audioCtx.destination)
     draw();
-  })
+  });
 
   function draw() {
     canvas.clearRect(0, 0, WIDTH, HEIGHT);
 
     analyser.getByteFrequencyData(dataArray);
-    console.log(dataArray)
 
-    canvas.fillStyle = "#fff";
+    canvas.fillStyle = "#000";
     canvas.fillRect(0, 0, WIDTH, HEIGHT);
 
     canvas.lineWidth = 2;
@@ -47,16 +45,15 @@ function listenerVisual() {
 
     let barWidth = (WIDTH * 1.0) / bufferLength;
     let barHeight = 0;
-    let heightScale = HEIGHT/128;
+    let heightScale = HEIGHT / 256;
     let x = 0;
 
-    for(var i = 0; i < bufferLength; i++) {
+    for (var i = 0; i < bufferLength; i++) {
       barHeight = dataArray[i];
 
-
-      canvas.fillStyle = 'rgb(' + (barHeight+0) + ',10,160)';
+      canvas.fillStyle = `rgb(${barHeight * 1}, 203, 52)`;
       barHeight *= heightScale;
-      canvas.fillRect(x, HEIGHT-barHeight/2, barWidth, barHeight/2);
+      canvas.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
 
       x += barWidth + 2;
     }
@@ -66,7 +63,6 @@ function listenerVisual() {
 
     requestAnimationFrame(draw);
   }
-
 }
 
-listenerVisual()
+listenerVisual();
